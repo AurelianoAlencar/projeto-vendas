0bade8e6887fe4cb6214244577a7d909c18125ba
AurelianoAlencar <alencarsobreira@terra.com.br>
2022-05-20 17:12:03 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
+import DataTable from "componets/DataTable";
+import Footer from "componets/Footer";
+import NavBar from "componets/NavBar";
 
 
 
 function App() {
   return (
-    <div >
-      <h1 className="text-primary"> Olá mundo!</h1>
-      
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary" >  Olá mundo!</h1>
+
+      <DataTable/>
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/componets/Footer/index.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+
+    return (
+
+<footer className="footer mt-auto py-3 bg-dark">
+  <div className="container">
+    <p className="text-light">App desenvolvido por <a href="https://github.com/AurelianoAlencar" target="_blank" rel="noreferrer">Aureliano Alencar</a></p>
+    <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+      Evento Semana Spring React: <a href="https://github.com/AurelianoAlencar" target="_blank" rel="noreferrer">Aureliano Alencar</a></small></p>
+  </div>
+</footer>
+
+    );
+}
+
+export default Footer;
