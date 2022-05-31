5aa932473f4ba0c33889ece7b6d92cd1eade00ea
AurelianoAlencar <alencarsobreira@terra.com.br>
2022-05-31 20:46:35 -0300

    Static charts

--- frontend/src/componets/DataTable/index.tsx
+++ frontend/src/componets/DataTable/index.tsx
@@ -30,6 +30,7 @@ const DataTable = () => {
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
+
                     <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
@@ -72,6 +73,12 @@ const DataTable = () => {
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
+
+
+
+
+
+
 
                 </tbody>
 
